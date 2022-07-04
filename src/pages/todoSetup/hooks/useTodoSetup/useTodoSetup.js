2a69559f6ab8d3f5c05a231f883188917f3b2539
shivamgarg08 <shgarg@tekion.com>
2022-07-04 13:01:16 +0530

    added optimisation

--- src/pages/todoSetup/hooks/useTodoSetup/useTodoSetup.js
+++ src/pages/todoSetup/hooks/useTodoSetup/useTodoSetup.js
@@ -1,5 +1,5 @@
 // Hooks
-import {useState } from 'react';
+import {useState, useCallback } from 'react';
 
 // Lodash
 import _map from 'lodash/map';
@@ -10,18 +10,18 @@ import { createTodoItem } from './useTodoSetup.helpers'
 function useTodoSetup(todoList, updateTodoList, saveTodoList) {
     const [todoText, setTodoText] = useState("");
 
-       const handleTodoTextChange = (e) => {
+       const handleTodoTextChange = useCallback((e) => {
             setTodoText(e.target.value);
-        }
+        },[]);
 
-        const handleTodoItemSave = (e) => {
+        const handleTodoItemSave = useCallback((e) => {
             e.preventDefault();
             if (!todoText) return;
              
             const newTodoInfo = createTodoItem(todoList, todoText);
             updateTodoList(newTodoInfo);
             setTodoText("");
-        }
+        },[todoList, todoText]);
 
         const isCompletedTodoList = (selectedId) => todoInfo => {
             if(todoInfo.id === selectedId)
@@ -29,10 +29,10 @@ function useTodoSetup(todoList, updateTodoList, saveTodoList) {
             return todoInfo;
         }
     
-        const onTodoItemClick = (selectedId) => {
+        const onTodoItemClick = useCallback((selectedId) => {
             const newTodoList = _map(todoList, isCompletedTodoList(selectedId));
             saveTodoList(newTodoList);
-        }
+        },[todoList])
 
     return {todoText, handleTodoTextChange, handleTodoItemSave, onTodoItemClick}
 }
